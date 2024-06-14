9c1046e1f7da4c23fd60c432b56b27be8b103072
Vbello-Tech <69384115+vbello-tech@users.noreply.github.com>
2024-06-14 20:06:52 +0100

    fixed allowed origi error

--- Auth/settings.py
+++ Auth/settings.py
@@ -18,7 +18,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('debug')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '0.0.0.0',
+    'localhost',
+    '127.0.0.1',
+]
 
 # Application definition
 
